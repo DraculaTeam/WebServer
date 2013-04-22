71a322743a16a24f8dba37361f8d6501d116b93e
RaviSharma <ravishar@thoughtworks.com>
2013-04-22 15:30:53 +0530

    Gaurav,Ravi|mockito-all jar added

--- dracula/test/com/dracula/webserver/WebServerTest.java
+++ dracula/test/com/dracula/webserver/WebServerTest.java
@@ -12,11 +12,11 @@ public class WebServerTest {
 
     @Test
     public void shouldConnectToTheServer() throws IOException {
-        WebServer server = new WebServer(new ServerSocket(9090));
+        WebServer server = new WebServer(new ServerSocket(8081));
         WebServer mock = Mockito.mock(WebServer.class);
         Mockito.doNothing().when(mock).start();
         mock.start();
-        Socket client = new Socket("localhost",9090);
+        Socket client = new Socket("localhost",8081);
         Mockito.verify(mock).start();
     }
 
